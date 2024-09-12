b1f87c1e8adb6f4934fe01d7009c679751c37ffd
Prashanth <prashanthvgowda1999@gmail.com>
2024-09-12 21:53:47 +0530

    route changes in delete method

--- routes/routes.go
+++ routes/routes.go
@@ -6,10 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Routes(r *gin.Engine)  {
+func Routes(r *gin.Engine) {
 	r.POST("/students", controller.Addstudentdetails)
 	r.GET("/students/:id", controller.GetStudentsdetails)
 	r.GET("/students", controller.Addstudentdetails)
 	r.PUT("/students/:id", controller.GetStudentsdetails)
-	r.DELETE("//students/:id", controller.GetStudentsdetails)
+	r.DELETE("/students/:id", controller.GetStudentsdetails)
 }
